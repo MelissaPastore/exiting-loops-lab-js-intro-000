9ac5f5968e9afecc9015d2052d34218982e1c283
Melissa Pastore <melissa.pastore@gmail.com>
2020-06-26 19:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function keepGoing (array, changeValue, skipValue){
 function findBy (array, findFn) {
  for(let i=0; i<array.length; i++){
    if(array[i] === findFn(i)){
-     return findFn;
+     return array[i];
    }
  }
  return null; 
